6b3471216b4eb0731c66e693de3d986ddd8161df
yasin vural <yasinvural0@gmail.com>
2020-02-08 19:44:38 +0300

    Updates

---
+++ precache-manifest.d83de7f995a0345105c89912b54f9fe9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "567eafe8662aad5e8d5988c28a76d23e",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "1b2563a5a22f67e94964",
+    "url": "/pokemon/static/css/2.1e105a3b.chunk.css"
+  },
+  {
+    "revision": "4462dec0b6f436c93ffa",
+    "url": "/pokemon/static/css/main.8bb7f02e.chunk.css"
+  },
+  {
+    "revision": "1b2563a5a22f67e94964",
+    "url": "/pokemon/static/js/2.0ade2ef6.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/pokemon/static/js/2.0ade2ef6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4462dec0b6f436c93ffa",
+    "url": "/pokemon/static/js/main.fb838ba9.chunk.js"
+  },
+  {
+    "revision": "4d01ac65a9746054ee98",
+    "url": "/pokemon/static/js/runtime-main.7cd9c6ff.js"
+  }
+]);
